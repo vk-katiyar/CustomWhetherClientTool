33c2d62ae098584d63786241719b68017782ac97
Vishal Katiyar <58584967+vk-katiyar@users.noreply.github.com>
2023-01-30 23:15:08 +0530

    Add files via upload

---
+++ CustomWeatherClientTool/CustomWeatherClientTool/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace CustomWeatherClientTool
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Name of the City.....");
+
+            string cityName = Console.ReadLine();
+
+            List<City> cities = LoadJson();
+            City city = null;
+
+            if (cities.Any())
+            {
+                city = cities.FirstOrDefault(x => x.city == cityName);
+            }
+
+            if(city != null)
+            {
+                GetCityWeatherDetails(city);
+            }
+        }
+
+        public static void GetCityWeatherDetails(City city)
+        {
+            var latitude = city.lat;
+            var longitude = city.lng;
+
+            string URL = "https://api.open-meteo.com/v1/forecast";
+            string urlParameters = "?latitude=" + latitude + "&longitude=" + longitude + "&current_weather=true";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                Response data = JsonConvert.DeserializeObject<Response>(result);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Weather Details of {0} are", city.city);
+                Console.WriteLine("Temperature --> {0}", data?.current_weather?.temperature);
+                Console.WriteLine("WindSpeed --> {0}", data?.current_weather?.windspeed);
+                Console.WriteLine("WindDirection --> {0}", data?.current_weather?.winddirection);
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            client.Dispose();
+        }
+
+        public static List<City> LoadJson()
+        {
+            List<City> cities = null;
+            string filePath = @"C:\Users\v.katiyar\source\repos\CustomWeatherClientTool\CustomWeatherClientTool\City.json";
+
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+            return cities;
+        }
+    }
+}
---
+++ CustomWeatherClientTool/CustomWeatherClientTool/ReadMe.md
@@ -0,0 +1,17 @@
+How to Install and Run this Application
+--------------------------------------------------------
+
+1) Download the Project and open the folder that contains Program.cs file in CMD.
+
+2) Copy the path of City.json file and replace in the line 65 in Program.cs file.
+
+3) After Opening the CMD type the command  -- dotnet build
+
+4) Once the build got succedded type the command -- dotnet run
+
+5) Once successfully running it will Ask for a city to Enter
+
+6) Enter the city name and see the Output.
+
+Thanks...!!
+	
