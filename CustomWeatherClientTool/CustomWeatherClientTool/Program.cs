01d7803c82ef1ecf24d50ea969cb1b18a5bfb0a9
Vishal Katiyar <58584967+vk-katiyar@users.noreply.github.com>
2023-01-30 23:30:23 +0530

    Update Program.cs

--- CustomWeatherClientTool/CustomWeatherClientTool/Program.cs
+++ CustomWeatherClientTool/CustomWeatherClientTool/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
@@ -9,65 +10,89 @@ namespace CustomWeatherClientTool
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Name of the City.....");
-
-            string cityName = Console.ReadLine();
-
-            List<City> cities = LoadJson();
-            City city = null;
-
-            if (cities.Any())
+            try
             {
-                city = cities.FirstOrDefault(x => x.city == cityName);
+                Console.WriteLine("Enter the Name of the City.....");
+
+                string cityName = Console.ReadLine();  // Reading City Name from User
+
+                List<City> cities = LoadJson();        // Loading Json for city with latitude and longitude
+                City city = null;
+
+                if (cities.Any())
+                {
+                    city = cities.FirstOrDefault(x => x.city == cityName);  // Getting city details 
+                }
+
+                if (city != null)
+                {
+                    GetCityWeatherDetails(city);
+                }
             }
-
-            if(city != null)
+            catch(Exception ex)
             {
-                GetCityWeatherDetails(city);
+                throw ex;
             }
         }
 
         public static void GetCityWeatherDetails(City city)
         {
-            var latitude = city.lat;
-            var longitude = city.lng;
-
-            string URL = "https://api.open-meteo.com/v1/forecast";
-            string urlParameters = "?latitude=" + latitude + "&longitude=" + longitude + "&current_weather=true";
-
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(URL);
-
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
-            if (response.IsSuccessStatusCode)
+            try
             {
-                string result = response.Content.ReadAsStringAsync().Result;
-                Response data = JsonConvert.DeserializeObject<Response>(result);
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("Weather Details of {0} are", city.city);
-                Console.WriteLine("Temperature --> {0}", data?.current_weather?.temperature);
-                Console.WriteLine("WindSpeed --> {0}", data?.current_weather?.windspeed);
-                Console.WriteLine("WindDirection --> {0}", data?.current_weather?.winddirection);
+                var latitude = city.lat;
+                var longitude = city.lng;
+
+                string URL = "https://api.open-meteo.com/v1/forecast";
+                string urlParameters = "?latitude=" + latitude + "&longitude=" + longitude + "&current_weather=true";
+
+                client.BaseAddress = new Uri(URL);
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Response data = JsonConvert.DeserializeObject<Response>(result);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Weather Details of {0} are", city.city);
+                    Console.WriteLine("Temperature --> {0}", data?.current_weather?.temperature);
+                    Console.WriteLine("WindSpeed --> {0}", data?.current_weather?.windspeed);
+                    Console.WriteLine("WindDirection --> {0}", data?.current_weather?.winddirection);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
             }
-            else
+            catch(Exception ex)
             {
-                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                throw ex;
             }
-            client.Dispose();
+            finally
+            {
+                client.Dispose();
+            }
         }
 
         public static List<City> LoadJson()
         {
             List<City> cities = null;
-            string filePath = @"C:\Users\v.katiyar\source\repos\CustomWeatherClientTool\CustomWeatherClientTool\City.json";
-
-            using (StreamReader r = new StreamReader(filePath))
+            try
             {
-                string json = r.ReadToEnd();
-                cities = JsonConvert.DeserializeObject<List<City>>(json);
+                string filePath = @"C:\Users\v.katiyar\source\repos\CustomWeatherClientTool\CustomWeatherClientTool\City.json";
+
+                using (StreamReader r = new StreamReader(filePath))
+                {
+                    string json = r.ReadToEnd();
+                    cities = JsonConvert.DeserializeObject<List<City>>(json);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
             }
             return cities;
         }
